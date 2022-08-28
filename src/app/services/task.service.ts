785a02f953c79465d5fb597a966fd3c85729d84a
Ahmad P. Evangelista <Ahmad.Evangelista@cebupacificair.com>
2022-08-28 11:01:32 +0800

    UI and Delete Task

---
+++ src/app/services/task.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Task } from '../Task';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TaskService {
+  private apuUrl = 'http://localhost:5000/tasks';
+  constructor(private http: HttpClient) {}
+
+  getTasks(): Observable<Task[]> {
+    return this.http.get<Task[]>(this.apuUrl);
+  }
+
+  deleteTask(task: Task): Observable<Task> {
+    const url = `${this.apuUrl}/${task.id}`;
+    return this.http.delete<Task>(url);
+  }
+}
